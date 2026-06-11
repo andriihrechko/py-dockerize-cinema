7255a954464d73e65d6b6eefe27ee1b6470e37c3
andriihrechko <andriihrechko250703@gmail.com>
2026-06-11 19:16:04 +0300

    refactor bad code

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.db import connections, OperationalError
 from psycopg2 import OperationalError as PsycopgError
+import time
 
 
 class Command(BaseCommand):
@@ -12,6 +13,5 @@ class Command(BaseCommand):
                 database_connection = connections["default"]
             except (OperationalError, PsycopgError):
                 self.stdout.write("Database unavailable, waiting 1 second...")
-                from datetime import time
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS("Database connected!"))
